96e83e1583e10ad15361be93080c4969e5742754
Stefani Kostic <stefanikostic6@gmail.com>
2021-04-26 19:14:06 +0200

    Removed the comments.

--- src/SensorNetwork3D.java
+++ src/SensorNetwork3D.java
@@ -385,26 +385,6 @@ public class SensorNetwork3D {
 
 
     public static Point3D Trilaterate (Point3D p1, Point3D p2, Point3D p3, Point3D p4) {
-        /*
-        p1=np.array(distances[0][:3])
-    p2=np.array(distances[1][:3])
-    p3=np.array(distances[2][:3])
-    p4=np.array(distances[3][:3])
-    r1=distances[0][-1]
-    r2=distances[1][-1]
-    r3=distances[2][-1]
-    r4=distances[3][-1]
-    e_x=(p2-p1)/np.linalg.norm(p2-p1)
-    i=np.dot(e_x,(p3-p1))
-    e_y=(p3-p1-(i*e_x))/(np.linalg.norm(p3-p1-(i*e_x)))
-    e_z=np.cross(e_x,e_y)
-    d=np.linalg.norm(p2-p1)
-    j=np.dot(e_y,(p3-p1))
-    x=((r1**2)-(r2**2)+(d**2))/(2*d)
-    y=(((r1**2)-(r3**2)+(i**2)+(j**2))/(2*j))-((i/j)*(x))
-
-         */
-        //var ex, ey, ez, i, j, d, a, x, y, z, p4;
         Point3D ex = vector_divide(vector_subtract(p2, p1), norm(vector_subtract(p2, p1)));
         double i = dot(ex, vector_subtract(p3, p1));
         Point3D a = vector_subtract(vector_subtract(p3, p1), vector_multiply(ex, i));
@@ -416,21 +396,11 @@ public class SensorNetwork3D {
         double x = (sqr(p1.r) - sqr(p2.r) + sqr(d)) / (2 * d);
         double y = (sqr(p1.r) - sqr(p3.r) + sqr(i) + sqr(j)) / (2 * j) - (i / j) * x;
         double z1 = Math.sqrt(sqr(p1.r) - sqr(x) - sqr(y));
-/*
-z1=np.sqrt(r1**2-x**2-y**2)
-    z2=np.sqrt(r1**2-x**2-y**2)*(-1)
-    ans1=p1+(x*e_x)+(y*e_y)+(z1*e_z)
-    ans2=p1+(x*e_x)+(y*e_y)+(z2*e_z)
-    dist1=np.linalg.norm(p4-ans1)
-    dist2=np.linalg.norm(p4-ans2)
-    if np.abs(r4-dist1)<np.abs(r4-dist2):
-        return ans1
-    else:
-        return ans2
- */
+
         double z2 = Math.sqrt(sqr(p1.r) - sqr(x) - sqr(y)) * (-1);
         Point3D ans1 = vector_add(p1, vector_add(vector_add(vector_multiply(ex, x), vector_multiply(ey, y)), vector_multiply(ez, z1)));
         Point3D ans2 = vector_add(p1, vector_add(vector_add(vector_multiply(ex, x), vector_multiply(ey, y)), vector_multiply(ez, z2)));
+
         double distance1 = norm(vector_subtract(p4, ans1));
         double distance2 = norm(vector_subtract(p4, ans2));
         if (Math.abs(p4.r - distance1) < Math.abs(p4.r - distance2)) {
